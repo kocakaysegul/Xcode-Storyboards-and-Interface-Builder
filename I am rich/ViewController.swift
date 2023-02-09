c6ddc8ba060e22e7bd9b9d97890731ddd6152a6d
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-09 13:07:22 +0300

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Ayşegül Koçak on 9.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
